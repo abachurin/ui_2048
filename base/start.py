b0e14c0647c35fa34d5685083c893c9e4e8525c8
Alex Bachurin <bachurin.alex@gmail.com>
2022-12-20 17:42:49 +0000

    *

--- base/start.py
+++ base/start.py
@@ -138,7 +138,7 @@ LOCAL = os.environ.get('S3_URL', 'local')
 if LOCAL == 'local':
     ROOT_URL = 'http://localhost:5000'
 else:
-    ROOT_URL = 'back_2048:5000'
+    ROOT_URL = 'http://back:5000'
 
 s3_credentials = {
     'region': os.environ.get('S3_REGION', None),
